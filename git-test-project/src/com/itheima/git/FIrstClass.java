9b58c58574607d84e63449cf9e40e96565a5bb58
huyaqiang <huyaqiangcd@163.com>
2021-07-19 12:02:43 +0800

    工程git-test-project添加test方法

--- git-test-project/src/com/itheima/git/FIrstClass.java
+++ git-test-project/src/com/itheima/git/FIrstClass.java
@@ -13,4 +13,9 @@ public class FIrstClass {
         System.out.println("hello git project");
         System.out.println("hello git project");
     }
+
+    public void test(){
+        int i=0;
+        i+=10;
+    }
 }
